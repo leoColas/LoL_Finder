500cb8ec6eaa67e16504ce3a3285af1ea01c6a69
leoColas <leocolas@protonmail.com>
2022-06-10 19:35:12 +0200

    Version 0.4

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var summoner = "Ulamog23";
+var summoner = prompt("Ton pseudo");
 var region = "euw1";
 
 const getSummonerData = async (summoner,region) => {
 
     //Global
-    const APIkey = "RGAPI-42021622-052f-4515-a7ec-1681ab8c5a85";
+    const APIkey = "RGAPI-35adbccf-5a21-4c09-ba25-f230e7ceb7c9";
     const nameArea = document.getElementById('name')
     const lvlArea = document.getElementById('lvl')
 
@@ -14,7 +14,7 @@ const getSummonerData = async (summoner,region) => {
     let globalData = await res.json()
 
     nameArea.innerHTML = `<p class="white">${globalData.name}</p>`
-    lvlArea.innerHTML = `<p class="white">Niveau ${globalData.summonerLevel}</p>`
+    lvlArea.innerHTML = `<p class="white">Niveau <span class="red">${globalData.summonerLevel}</span></p>`
 
     const getIcon = async () => {
         const iconImg = document.getElementById('iconB')
@@ -49,7 +49,7 @@ const getSummonerData = async (summoner,region) => {
         }
 
         const getIconFromChamp = (champName) => {
-            return `<img class="border" src='https://ddragon.leagueoflegends.com/cdn/12.10.1/img/champion/${champName}.png' alt="">`
+            return `<img class="p" src='https://ddragon.leagueoflegends.com/cdn/12.10.1/img/champion/${champName}.png' alt="">`
         }
 
         data2.data = Object.entries(data2.data);
@@ -58,9 +58,9 @@ const getSummonerData = async (summoner,region) => {
         mastery1 = getChampNameFromId(mastery1)
         mastery2 = getChampNameFromId(mastery2)
 
-        masteryB1.innerHTML = `${getIconFromChamp(mastery0)}<p class="center">${mastery0}</p><p class="center">Niveau ${masteryLvl0}</p>`
-        masteryB2.innerHTML = `${getIconFromChamp(mastery1)}<p class="center">${mastery1}</p><p class="center">Niveau ${masteryLvl1}</p>`
-        masteryB3.innerHTML = `${getIconFromChamp(mastery2)}<p class="center">${mastery2}</p><p class="center">Niveau ${masteryLvl2}</p>`
+        masteryB1.innerHTML = `<img class="c" src="https://www.masterypoints.com/assets/img/lol/mastery_profile/${masteryLvl0}.png" alt="">${getIconFromChamp(mastery0)}<p class="center big">${mastery0}</p><p class="center">Niveau <span class="red">${masteryLvl0}</span></p>`
+        masteryB2.innerHTML = `<img class="c" src="https://www.masterypoints.com/assets/img/lol/mastery_profile/${masteryLvl1}.png" alt="">${getIconFromChamp(mastery1)}<p class="center big">${mastery1}</p><p class="center">Niveau <span class="red">${masteryLvl1}</span></p>`
+        masteryB3.innerHTML = `<img class="c" src="https://www.masterypoints.com/assets/img/lol/mastery_profile/${masteryLvl2}.png" alt="">${getIconFromChamp(mastery2)}<p class="center big">${mastery2}</p><p class="center">Niveau <span class="red">${masteryLvl2}</p>`
 
     }
 
@@ -69,5 +69,3 @@ const getSummonerData = async (summoner,region) => {
 }
 
 getSummonerData(summoner,region)
-
-//https://www.masterypoints.com/assets/img/lol/mastery_profile/4.png
